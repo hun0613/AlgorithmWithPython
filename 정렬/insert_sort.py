05c1566266092510970c86f63005f49232dafdae
hun0613 <pshun7614@gmail.com>
2025-03-14 14:20:09 +0900

    정렬 알고리즘

--- 정렬/insert_sort.py
+++ 정렬/insert_sort.py
@@ -1,5 +1,15 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+#
+# for i in range(1, len(array)):
+#     for j in range(i, 0, -1):
+#         if array[j] < array[j - 1]:
+#             array[j], array[j - 1] = array[j - 1], array[j]
+#         else:
+#             break
+#
+# print(array)
 
+# 왼쪽은 이미 정렬이 끝난 상태로 간주
 for i in range(1, len(array)):
     for j in range(i, 0, -1):
         if array[j] < array[j - 1]:
@@ -8,3 +18,5 @@ for i in range(1, len(array)):
             break
 
 print(array)
+
+# O(N^2 ~ N)
--- 정렬/select_sort.py
+++ 정렬/select_sort.py
@@ -1,5 +1,15 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
+# for i in range(len(array)):
+#     min_index = i
+#     for j in range(i + 1, len(array)):
+#         # 가장 작은 수를 찾아 내는 과정
+#         if array[min_index] > array[j]:
+#             min_index = j
+#     array[i], array[min_index] = array[min_index], array[i]
+#
+# print(array)
+
 for i in range(len(array)):
     min_index = i
     for j in range(i + 1, len(array)):
@@ -7,4 +17,7 @@ for i in range(len(array)):
             min_index = j
     array[i], array[min_index] = array[min_index], array[i]
 
+
 print(array)
+
+#O(n^2)
